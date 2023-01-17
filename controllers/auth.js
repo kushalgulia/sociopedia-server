dee1b5112b5c0d2d1617679cd65b1fdb30877514
kushalgulia <kushalgulia896@gmail.com>
2023-01-18 00:44:53 +0530

    debug

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
 
-/* Register user*/
+/* REGISTER USER */
 export const register = async (req, res) => {
   try {
     const {
@@ -14,41 +14,42 @@ export const register = async (req, res) => {
       friends,
       location,
       occupation,
-    } = req.body // deconstruct the request
+    } = req.body
 
     const salt = await bcrypt.genSalt()
-    const passowrdHash = await bcrypt.hash(password, salt)
+    const passwordHash = await bcrypt.hash(password, salt)
+
     const newUser = new User({
       firstName,
       lastName,
       email,
-      password: passowrdHash,
+      password: passwordHash,
       picturePath,
       friends,
       location,
       occupation,
-      viewedProfile: Math.floor(Math.random() * 100),
-      impressions: Math.floor(Math.random() * 1000),
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
     })
-    const savedUser = await newUser.save() // saves a new user
-    res.status(201).json(savedUser) // sends the user to the front end
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
   } catch (err) {
-    res.status(500).json({ error: err.message }) //sends error sent by mongodb to the front end
+    res.status(500).json({ error: err.message })
   }
 }
 
-/* Logging in*/
+/* LOGGING IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body
     const user = await User.findOne({ email: email })
-    if (!user) return res.status(400).json({ msg: 'User does not exist' })
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' })
 
-    const isMatch = await bcrypt.compare(password, user.passowrd)
-    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' })
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials. ' })
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-    delete user.passowrd
+    delete user.password
     res.status(200).json({ token, user })
   } catch (err) {
     res.status(500).json({ error: err.message })
